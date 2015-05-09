02970d63acd822e2f5161c426562169199f3b03f
EngrishMajor <ken.yht@gmail.com>
2015-05-08 19:43:26 -0500

    created post and finished blog models, finished user CRUD

--- models/blog.js
+++ models/blog.js
@@ -1,8 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , Post = mongoose.model('Post');
 
-var Blog = new mongoose.Schema({
-	authorId: { type: Schema.Types.ObjectId },
-	authorName: { type: String }
+var Blog = new Schema({
+	authorId	: { type: Schema.Types.ObjectId },
+	authorName	: { type: String },
+	posts 		: { type: [ Post ] },
+	timeCreated	: { type: Date },
+	lastUpdated	: { type: Date },
+	title		: { type: String }
 });
 
 module.exports = mongoose.model('Blog', Blog);
---
+++ routes/blogs.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose')
+  , User = mongoose.model('User')
+  , Blog = mongoose.model('Blog')
+  , Post = mongoose.model('Post');
+
+exports.createBlog = function(req, res) {
+
+};
+
+exports.listBlogs = function(req, res) {
+
+};
+
+exports.updateBlog = function(req, res) {
+
+};
+
+exports.deleteBlog = function(req, res) {
+
+};
+
+exports.createPost = function(req, res) {
+
+};
+
+exports.listPosts = function(req, res) {
+
+};
+
+exports.updatePost = function(req, res) {
+
+};
+
+exports.deletePost = function(req, res) {
+
+};
